f16768fd3bbf332d3bd18087ebc578d766efa518
Aline Diani <63258951+alinediani@users.noreply.github.com>
2020-09-29 23:37:15 -0300

    adição de segundo card

--- card/src/App.jsx
+++ card/src/App.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
 import Card from './componentes/card'
+import './index.css'
 
 export default()=>(
     <div id="app">
         <h1>Login</h1>
+       <div className='a'>
         <Card titulo="Exemplo de card"></Card>
-    </div>
+        <Card titulo='2'></Card>
+        </div>
+        </div>
+ 
 )
--- card/src/componentes/card.jsx
+++ card/src/componentes/card.jsx
@@ -2,9 +2,11 @@ import React from 'react'
 import './card.css'
 export default props=>{
     return(
+        
         <div className='Card'>
            <div className="title">{props.titulo}</div>
            <div className='content'>Conteúdo</div>
         </div>
+       
     )
 }
