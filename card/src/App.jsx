9f4cb7464c3542ad69365e295e137b98aba42316
Aline Diani <63258951+alinediani@users.noreply.github.com>
2020-10-04 03:01:04 -0300

    alterações

--- card/src/App.jsx
+++ card/src/App.jsx
@@ -1,13 +1,18 @@
 import React from 'react';
 import Card from './componentes/card'
+import Navbar from './componentes/navbar'
 import './index.css'
 
 export default()=>(
-    <div id="app">
+    <div >
+        <header>
+            <Navbar></Navbar>
+        </header>
         <h1>Login</h1>
        <div className='a'>
         <Card titulo="Exemplo de card"></Card>
         <Card titulo='2'></Card>
+        <Card titulo='Aline e Cainan'></Card>
         </div>
         </div>
  
---
+++ card/src/componentes/navbar.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import './card.css'
+export default props2=>{
+    return(
+        <nav>
+            <a href="#">Login</a>
+        </nav>
+    )
+}
